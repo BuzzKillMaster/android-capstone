a4975802e8f3780a1867a15cd8555cdde527e52c
Christian Pedersen <chris@buzzkill.dev>
2023-05-27 08:58:40 +0200

    create welcome screen

--- app/src/main/java/com/example/littlelemoncapstone/WelcomeActivity.kt
+++ app/src/main/java/com/example/littlelemoncapstone/WelcomeActivity.kt
@@ -1,15 +1,29 @@
 package com.example.littlelemoncapstone
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.littlelemoncapstone.ui.theme.LittleLemonCapstoneTheme
 
 class WelcomeActivity : ComponentActivity() {
@@ -17,22 +31,62 @@ class WelcomeActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LittleLemonCapstoneTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting2("Android")
+                    WelcomeScreen(beginOnboarding = { beginOnboarding() })
                 }
             }
         }
     }
+
+    private fun beginOnboarding() {
+        val intent = Intent(this, OnboardingActivity::class.java)
+        startActivity(intent)
+    }
 }
 
 @Composable
-fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun WelcomeScreen(beginOnboarding: () -> Unit = {}) {
+    Column(
+        modifier = Modifier.fillMaxSize().padding(16.dp)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.weight(1f)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = null,
+                modifier = Modifier.fillMaxWidth(0.5f)
+            )
+
+            Text(
+                text = "Welcome to Little Lemon",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 36.dp, bottom = 24.dp)
+            )
+
+            Text(
+                text = "The ultimate destination for authentic Mediterranean food and experiences.",
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+        }
+
+        Button(
+            onClick = { beginOnboarding() },
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Text("Get Started")
+        }
+    }
 }
