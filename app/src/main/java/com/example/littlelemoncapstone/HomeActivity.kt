091c67817582b7ef0d772a1e5146ede26315824d
Christian Pedersen <chris@buzzkill.dev>
2023-05-27 10:23:01 +0200

    create homescreen header

--- app/src/main/java/com/example/littlelemoncapstone/HomeActivity.kt
+++ app/src/main/java/com/example/littlelemoncapstone/HomeActivity.kt
@@ -4,8 +4,13 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
@@ -19,7 +24,12 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.littlelemoncapstone.ui.theme.LittleLemonCapstoneTheme
 
 class HomeActivity : ComponentActivity() {
@@ -65,5 +75,47 @@ class HomeActivity : ComponentActivity() {
 
 @Composable
 fun HomeScreen() {
-    Text(text = "Home Screen")
+    Column {
+        HomeScreenHeader()
+    }
+}
+
+@Composable
+fun HomeScreenHeader() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color(0xFF495E57))
+            .padding(16.dp)
+    ) {
+        Row {
+            Column(
+                modifier = Modifier.weight(0.8f)
+            ) {
+                Text(
+                    text = "Little Lemon",
+                    color = Color(0xFFF4CE14),
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = "Chicago",
+                    color = Color(0xFFEDEFEE),
+                    fontSize = 20.sp,
+                    modifier = Modifier.padding(top = 8.dp, bottom = 16.dp)
+                )
+
+                Text(
+                    text = "We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.",
+                    color = Color(0xFFEDEFEE),
+                )
+            }
+
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = null,
+                modifier = Modifier.weight(0.2f)
+            )
+        }
+    }
 }
