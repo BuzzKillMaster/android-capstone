fdcd562bd65287600d701b98ca3e99e3dc3df0bf
Christian Pedersen <chris@buzzkill.dev>
2023-05-27 10:12:10 +0200

    create profile screen and enable signout

--- app/src/main/java/com/example/littlelemoncapstone/HomeActivity.kt
+++ app/src/main/java/com/example/littlelemoncapstone/HomeActivity.kt
@@ -1,46 +1,69 @@
 package com.example.littlelemoncapstone
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.littlelemoncapstone.ui.theme.LittleLemonCapstoneTheme
 
 class HomeActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             LittleLemonCapstoneTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Scaffold(
+                        topBar = {
+                            TopAppBar(
+                                title = { Text("Little Lemon") },
+                                actions = {
+                                    IconButton(onClick = { launchProfileActivity() }) {
+                                        Icon(Icons.Filled.Person, contentDescription = "Profile")
+                                    }
+                                }
+                            )
+                        }
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(it)
+                        ) {
+                            HomeScreen()
+                        }
+                    }
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LittleLemonCapstoneTheme {
-        Greeting("Android")
+    private fun launchProfileActivity() {
+        val intent = Intent(this, ProfileActivity::class.java)
+        startActivity(intent)
     }
+}
+
+@Composable
+fun HomeScreen() {
+    Text(text = "Home Screen")
 }
--- app/src/main/java/com/example/littlelemoncapstone/ProfileActivity.kt
+++ app/src/main/java/com/example/littlelemoncapstone/ProfileActivity.kt
@@ -1,38 +1,102 @@
 package com.example.littlelemoncapstone
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.littlelemoncapstone.models.User
 import com.example.littlelemoncapstone.ui.theme.LittleLemonCapstoneTheme
+import com.google.gson.Gson
 
 class ProfileActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val preferences = getSharedPreferences("LittleLemon", MODE_PRIVATE)
+
+        val user = preferences.getString("user", null)?.let {
+           Gson().fromJson(it, User::class.java)
+        } ?: throw IllegalStateException("User not found")
+
         setContent {
             LittleLemonCapstoneTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting4("Android")
+                    Scaffold(
+                        topBar = {
+                            TopAppBar(
+                                title = { Text("Your profile") },
+                                navigationIcon = {
+                                    IconButton(onClick = { finish() }) {
+                                        Icon(Icons.Filled.ArrowBack, contentDescription = "Back")
+                                    }
+                                }
+                            )
+                        }
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(it)
+                        ) {
+                            ProfileScreen(user = user, signOut = { signOut() })
+                        }
+                    }
                 }
             }
         }
     }
+
+    private fun signOut() {
+        val preferences = getSharedPreferences("LittleLemon", MODE_PRIVATE)
+        preferences.edit().remove("user").apply()
+
+        val intent = Intent(this, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(intent)
+    }
 }
 
 @Composable
-fun Greeting4(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ProfileScreen(user: User, signOut: () -> Unit = {}) {
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Column(
+            modifier = Modifier.weight(1f)
+        ) {
+            Text(text = "Hello, ${user.name}!")
+            Text(text = "Your email is: ${user.email}")
+        }
+
+        Button(
+            onClick = { signOut() },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Sign out")
+        }
+    }
 }
