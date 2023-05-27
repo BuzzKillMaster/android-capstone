7ff9e0e02afc69de46b82e041bdb4e551d3840a2
Christian Pedersen <chris@buzzkill.dev>
2023-05-27 08:46:55 +0200

    check if user is signed in and start the proper intent

--- app/src/main/java/com/example/littlelemoncapstone/MainActivity.kt
+++ app/src/main/java/com/example/littlelemoncapstone/MainActivity.kt
@@ -1,46 +1,23 @@
 package com.example.littlelemoncapstone
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.littlelemoncapstone.ui.theme.LittleLemonCapstoneTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            LittleLemonCapstoneTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+
+        val preferences = getSharedPreferences("LittleLemon", MODE_PRIVATE)
+        val user = preferences.getString("user", null)
+
+        val intent = if (user == null) {
+            Intent(this, WelcomeActivity::class.java)
+        } else {
+            Intent(this, HomeActivity::class.java)
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LittleLemonCapstoneTheme {
-        Greeting("Android")
+        startActivity(intent)
+        finish()
     }
 }
